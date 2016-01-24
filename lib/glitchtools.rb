49fe9a72f6787631ca6ce1ead2f459697975c775
schizoakustik <schizoakustik@schizoakustik.se>
2016-01-24 20:04:02 +0100

    small fixes

--- lib/glitchtools.rb
+++ lib/glitchtools.rb
@@ -12,9 +12,10 @@ module Glitchtools
       frame_types.each_with_index { |i, n| puts "#{n} is_iframe" if i }
     end
   end
-  
+
   class JoinerAndMosher
     def initialize *files
+      # Check to see of given file is avi, otherwise convert it
       files.each_with_index do |f, i|
         unless File.extname(f) == ".avi"
           fn = File.basename(f, '.*')
@@ -29,20 +30,14 @@ module Glitchtools
       k = 0
       a1 = AviGlitch.open files[0]
       a2 = AviGlitch.open files[1]
+      # Keep frames from first file, end on keyframe
       a1.frames.each_with_index do |f, i|
         k = i if f.is_keyframe?
       end
       a1.frames = a1.frames[k, 1]
-       #Glitching thread
+      #Glitching thread
       t1 = Thread.new{
-        #a.concat(a2.frames)
-        #p = AviGlitch.open a
-        #q = []
-        # Keep first keyframe
-        #q = p.frames[0, 1]
-        # To hell with the rest
         a2.remove_all_keyframes!
-        # Tack the rest of the (all now non-key)frames to the end of q
         a1.frames.concat(a2.frames)
         o = AviGlitch.open a1.frames
         # Check to see if file exists, increment filename
@@ -89,11 +84,11 @@ module Glitchtools
         :trailing_frames => params[3],
         :repetitions => params[4]
       }
-      unless File.extname(options[:file]) == ".avi"
-          fn = File.basename(options[:file], '.*')
+      unless File.extname( options[:file] ) == ".avi"
+          fn = File.basename( options[:file], '.*' )
           f = FFMPEG::Movie.new(options[:file])
-          f.transcode("#{fn}.avi", "-r 25 -c:v libxvid -bf 2 -level 5")
-          options[:file] = "#{fn}.avi"
+          f.transcode( "#{ fn }.avi", "-r 25 -c:v libxvid -bf 2 -level 5" )
+          options[:file] = "#{ fn }.avi"
         end
       repeat_frames options
     end
@@ -106,25 +101,21 @@ module Glitchtools
       # Glitching thread
       t1 = Thread.new {
         # Get rid of file ext for later
-        filename = File.basename(options[:file], '.*')
+        filename = File.basename( options[:file], '.*' )
         # Get the last frame
         last_frame = file.frames.size
         # Put all non-keyframes in d
         d = []
         file.frames.each_with_index { |f, i| d.push(i) if f.is_deltaframe? }
         # Keep frames up until last_buffer_frame
-        q = file.frames[0, last_buffer_frame]            # TODO a similar construct as below i.e. file.frames[0, @lbf].to_avi.frames.concat ... ?
-        # Add frame_to_repeat and any trailing_frames to x
-        x = file.frames[d[frame_to_repeat], trailing_frames]
-        # Repeat frames repetitions times and add to q
-        q.concat(x * repetitions)
+        f = file.frames[0, last_buffer_frame].to_avi.frames.concat( file.frames[d[frame_to_repeat], trailing_frames] * repetitions )
         # Add the rest of the frames to y and stick it to the end of q
         y = file.frames[d[last_buffer_frame + 1], last_frame - last_buffer_frame - 1]
         y = AviGlitch.open(y)
         y.remove_all_keyframes!
-        q.concat(y.frames)
+        f.concat(y.frames)
         # New AviGlitch instance with the glitched file
-        o = AviGlitch.open(q)
+        o = AviGlitch.open(f)
         # Save glitched file
         @outfile = "#{filename}_[0-#{last_buffer_frame}_((#{frame_to_repeat}-#{trailing_frames})x#{repetitions})]"
         o.output "#{@outfile}.avi"
@@ -152,3 +143,6 @@ module Glitchtools
     end
   end
 end
+
+Dir.chdir("/home/linus/Videos/glitch/lejon_andy/")
+Glitchtools::Framerepeater.new(*%w"andy_02.avi 3 3 1 15")
